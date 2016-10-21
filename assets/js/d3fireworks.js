3c5dd040f19517ed5b43cebae5a66e9c4aa6e867
lexdacurro <lexdacurro@gmail.com>
2016-10-21 18:55:25 -0400

    damn

--- assets/js/d3fireworks.js
+++ assets/js/d3fireworks.js
@@ -12,7 +12,7 @@ var w=window.innerWidth,
     refreshInterval=[]; */
 	
 /* my preferred function */
-    var w= 1246,
+    var w= window.innerWidth-50,
     h=300,
     z = d3.scale.category20b(),
     i = 0,
